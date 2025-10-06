ed50d4cf619c9633e044bb0edc33a9742caf6030
Luis Alberto Carrillo Parra <lacarrillo240604@gmail.com>
2025-10-06 05:50:21 -0600

    Chore: whitebox
    * Chore: add config
    
    Description
    - Initial repo setup and configs
    - linter added
    
    * chore: hook fixes
    
    Applied formating changes
    
    * chore: hook fix
    
    Pre-commit correction
    
    * chore: hw1
    
    Black box tests homework 1 exercises solved
    
    * style: trailing fix
    
    trailing whitespace problem on homework_1.txt corrected
    
    * fix: EOF fix
    
    End of file homework_1.txt fixed
    
    * feat: whitebox tests
    
    white box tests 1-3 solved
    
    * fix: test_class_exercises
    
    file reformatted
    
    * feat: New exercises
    
    Exercises 1-10 and 23 solved
    
    * fix:pre_commit
    
    test_class_exercises pre_commit problems solved
    
    * fix: pre_commit
    
    Second test_class_exercises pre_commit fix
    
    * fix: test_class
    
    pre_commit test class fix
    
    * feat: ex 27 & 28
    
    exercises 27 and 28 solved
    
    * fix:view_cart
    
    test_view_cart_with_items fixed
    
    * fix:view_cart
    
    pre-commit run
    
    * fix: danger
    
    danger file problems fix

--- white_box/test_class_exercises.py
+++ white_box/test_class_exercises.py
@@ -5,10 +5,17 @@ White-box unit testing examples.
 """
 import io
 import sys
+import io
+import sys
 import unittest
+from io import StringIO
 from io import StringIO
 
 from white_box.class_exercises import (
+    BankAccount,
+    BankingSystem,
+    Product,
+    ShoppingCart,
     BankAccount,
     BankingSystem,
     Product,
@@ -25,7 +32,6 @@ from white_box.class_exercises import (
     validate_password,
     verify_age,
 )
-
 
 class TestWhiteBoxCheckNumberStatus(unittest.TestCase):
     """
@@ -694,7 +700,6 @@ class TestTrafficLight(unittest.TestCase):
             "get_current_state should return 'Red'",
         )
 
-
 # Samuel Pia
 class TestBankingSystem(unittest.TestCase):
     """
@@ -942,13 +947,15 @@ class TestWhiteBoxShoppingCart(unittest.TestCase):
         self.cart.add_product(self.product_a, 2)
         self.cart.add_product(self.product_b, 1)
 
-        expected_msg = """\
-        2 x Shirt - $100.0
-        1 x Pants - $80.0"""
+        expected_lines = ["2 x Shirt - $100.0", "1 x Pants - $80.0"]
 
         captured = io.StringIO()
         sys.stdout = captured
         self.cart.view_cart()
         sys.stdout = sys.__stdout__
 
-        self.assertEqual(captured.getvalue().strip(), expected_msg)
+        output_lines = [
+            line.strip() for line in captured.getvalue().strip().splitlines()
+        ]
+        self.assertEqual(output_lines, expected_lines)
+
