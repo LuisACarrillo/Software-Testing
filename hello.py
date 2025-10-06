7c9fbc934c64ded66e4505ffe5f19764612dd8ea
Luis <lacarrillo240604@gmail.com>
2025-10-06 08:55:00 -0600

    fix: fixed
    fixed

--- hello.py
+++ hello.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 """Módulo de prueba que imprime un saludo."""
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 742e8cb (Chore: add config)
+
 def main():
     """Función principal."""
     print("Hello world")
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 742e8cb (Chore: add config)
 if __name__ == "__main__":
     main()
